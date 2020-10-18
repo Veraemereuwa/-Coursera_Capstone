ad50b311eb8261ec90bbd73918d82f001d6180dc
Vera Uzoamaka Emereuwa <68230253+Veraemereuwa@users.noreply.github.com>
2020-10-18 13:19:49 +0100

    Add files via upload

---
+++ Capstone project notebook.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <p>  This notebook would be used to perform projects under THE APPLIED DATASCIENCE CAPSTONE</P>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
